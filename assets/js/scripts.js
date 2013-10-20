1789b4dbd1196411fbc3759132ce93c8fe79452d
chudepohl <chudepohl@MacBook-Pro-van-Chris.local>
2013-10-20 10:39:08 +0200

    Upload theme

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * Main JS file for Nerdy behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+		// Fit Vids
+        jQuery(".post").fitVids();
+
+    });
+
+}(jQuery));
